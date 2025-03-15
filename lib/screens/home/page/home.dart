04db35716aae00a5bca1e83e54ddc8a3ac22bb63
jannatul373 <ferdaus15-4982@diu.edu.bd>
2025-03-15 13:37:41 +0600

    2nd

--- lib/screens/home/page/home.dart
+++ lib/screens/home/page/home.dart
@@ -3,8 +3,22 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({super.key});
 
-  @override
+   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Mega shop")));
-  }
+    return Scaffold(
+      appBar: FAppBar(
+        title: "New Title",
+        backButton: true,
+      ),
+      body: Column(
+        children: [
+          // Poster Section
+          Container(
+            margin: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+            decoration: BoxDecoration(
+              color: Colors.blue, // Background color
+              borderRadius: BorderRadius.circular(12),
+            );
+}
 }
